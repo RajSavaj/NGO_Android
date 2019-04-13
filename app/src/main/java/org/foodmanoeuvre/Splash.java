73e0fa18e5f243034413fd3954bc64f2c5d9d776
Patel Raj <rsavaj7@gmail.com>
2019-04-14 01:07:49 +0530

    Changes

--- app/src/main/java/org/foodmanoeuvre/Splash.java
+++ app/src/main/java/org/foodmanoeuvre/Splash.java
@@ -1,5 +1,7 @@
 package org.foodmanoeuvre;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +10,15 @@ public class Splash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(getApplicationContext(),Login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
     }
 }
